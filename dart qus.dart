b67b5d93e82986297c9ee7237c7bad72fe25b814
MMG <muhammed0mishhal@gmail.com>
2026-01-06 14:43:59 +0530

    Increment & Decrement — Questions

---
+++ dart qus.dart
@@ -0,0 +1,24 @@
+//Increment & Decrement — Questions
+void main() {
+  int num1 = 5;
+  num1 = ++num1;
+  print("result is $num1");
+//Decrease it by 1
+  int num2=10;
+  num2=--num2;
+  print("result is $num2");
+  // Print a number, then increase it by 1, then print it again.
+  int num3=5;
+  print("the number is $num3");
+  num3=++num3;
+  print("reult is $num3");
+  //Decrease a number first, then print it
+  int num4=10;
+  num4=--num4;
+  print("the result is $num4");
+  // Use a loop to count from 1 to 5 by increasing the value each time.
+  int i=1;
+  for(i=1;i<=5;i++){
+    print("$i");
+  }
+}

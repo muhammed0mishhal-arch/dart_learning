44f8e51b8021f1a65a13a3e80e834eb446ee21d1
MMG <muhammed0mishhal@gmail.com>
2026-01-06 12:29:45 +0530

    i just updated example.dart with a print edit

--- example.dart
+++ example.dart
@@ -5,5 +5,5 @@ void main(){
   var sum=number1+number2+number3;
   print("the sum of the numbers is $sum");
   var mult=number1*number2;
-  print("the multiplication result of the number1&2 is $mult");
+  print("the multiplication result of the number1&2 is just  $mult");
 }

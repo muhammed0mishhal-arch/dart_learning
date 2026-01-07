d6d93b080d1640f527aa827f07f56a2c634ef28c
MMG <muhammed0mishhal@gmail.com>
2026-01-07 11:54:17 +0530

    new file uploaded

---
+++ logical_operators_task.dart
@@ -0,0 +1,27 @@
+void main(){
+  //Start with a number and add 3 to it using a shortcut operator.
+  int num=5;
+  num+=3;
+  print("the new num is $num");
+//Start with a number and subtract 4 using a shortcut operator.
+int num1=6;
+num1-=4;
+print("result is $num1");
+// Start with a number and multiply it by 2 using a shortcut operator
+int num2=10;
+num2*=2;
+print("the result is $num2");
+//Start with a number and divide it by 2 using a shortcut operator
+double num3=50;
+num3/=2;
+print("the result is $num3");
+// Start with a number and find the remainder when divided by 5 using a shortcut operator.
+double num4=8;
+num4%=5;
+print("the result is $num4");
+
+
+
+
+
+  }
